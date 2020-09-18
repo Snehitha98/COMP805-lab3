04449e0bca3e15bccc9c977c190e571903ff68c7
snehitha <sm1552@wildcats.unh.edu>
2020-09-18 13:03:28 -0400

    moved cheeses to everycheese folder

--- everycheese/everycheese/cheeses/models.py
+++ everycheese/everycheese/cheeses/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
 
 class Cheese(TimeStampedModel):
     name = models.CharField("Name of Cheese", max_length=255)
     slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from="name")
-
     description = models.TextField("Description", blank=True)
+
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
         SOFT = "soft", "Soft"
@@ -16,3 +17,6 @@ class Cheese(TimeStampedModel):
     # Other Fields Here...
 
     firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+
+    def __str__(self):
+        return self.name
